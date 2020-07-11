51be2d391b836ebe62f69d8eb5a7486bfc9dede5
doll <doll@192.168.1.102>
2020-07-11 21:55:02 +0530

    mutipe parse File

---
+++ FileRestContoller/src/main/java/com/file/FileRestContollerApplication.java
@@ -0,0 +1,13 @@
+package com.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileRestContollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileRestContollerApplication.class, args);
+	}
+
+}
